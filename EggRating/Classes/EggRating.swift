3e4b0078d8fb85cc2705766a56a9687a1cc66d4b
Somjintana K <nuisomjin@gmail.com>
2016-12-26 14:56:27 +0700

    Add remind period and first used condition to show rate view

--- EggRating/Classes/EggRating.swift
+++ EggRating/Classes/EggRating.swift
@@ -13,7 +13,10 @@ public class EggRating: NSObject {
     public static var itunesId = ""
     
     public static var minRatingToAppStore = 4.0
+    
     public static var daysUntilPrompt = 10
+    
+    public static var remindPeriod = 10
     
     // MARK: - Star Properties
     public static var starFillColor = UIColor(red: 255/255, green: 181/255, blue: 17/255, alpha: 1)
@@ -37,10 +40,16 @@ public class EggRating: NSObject {
     
     public static var shouldPromptForRating: Bool {
         
+        let daysFromFirstUsed = Calendar.current.dateComponents([.day], from: firstUsed, to: Date()).day ?? 0
         let daysFromLastRemind = Calendar.current.dateComponents([.day], from: lastRemind, to: Date()).day ?? 0
         
-        if daysFromLastRemind < daysUntilPrompt {
-            print("[EggRating] It's not the day until prompt yet.")
+        if daysFromFirstUsed < daysUntilPrompt {
+            print("[EggRating] Users have just used the app for only \(daysFromLastRemind) \(daysFromLastRemind <= 1 ? "day" : "days"). EggRating will be prompted in the next \(daysUntilPrompt - daysFromLastRemind) \(daysUntilPrompt - daysFromLastRemind <= 1 ? "day" : "days").")
+            return false
+        }
+        
+        if daysFromLastRemind < remindPeriod {
+            print("[EggRating] EggRating was just prompted last \(daysFromLastRemind) \(daysFromLastRemind <= 1 ? "day" : "days") ago. EggRating will be prompted again in the next \(remindPeriod - daysFromLastRemind) \(remindPeriod - daysFromLastRemind <= 1 ? "day" : "days").")
             return false
         }
         
@@ -50,6 +59,7 @@ public class EggRating: NSObject {
     public static func promptRateUsIfNeeded(viewController: UIViewController) {
         if shouldPromptForRating {
             self.promptRateUs(viewController: viewController)
+            lastRemind = Date()
         }
     }
     
@@ -75,7 +85,7 @@ public class EggRating: NSObject {
     
     // MARK: - Helper
     
-    // MARK: - View
+    // MARK: View
     
     private static var rateViewFromNib: UIViewController? {
         let podBundle = Bundle(for: EggRating.self)
@@ -95,9 +105,23 @@ public class EggRating: NSObject {
         return nil
     }
     
-    // MARK: cache
+    // MARK: Cache
     
     fileprivate static let userDefaults = UserDefaults.standard
+    
+    fileprivate static var firstUsed: Date {
+        set (date) {
+            userDefaults.set(date, forKey: EggRatingUserDefaultsKey.firstUsedKey.rawValue)
+        } get {
+            guard let firstUsedDate = userDefaults.object(forKey: EggRatingUserDefaultsKey.firstUsedKey.rawValue) as? Date else {
+                let today = Date()
+                userDefaults.set(today, forKey: EggRatingUserDefaultsKey.firstUsedKey.rawValue)
+                return today
+            }
+            
+            return firstUsedDate
+        }
+    }
     
     fileprivate static var lastRemind: Date {
         set(date) {
@@ -116,5 +140,6 @@ public class EggRating: NSObject {
 }
 
 enum EggRatingUserDefaultsKey: String {
+    case firstUsedKey = "EggRatingFirstUsed"
     case lastRemindKey = "EggRatingLastRemind"
 }
